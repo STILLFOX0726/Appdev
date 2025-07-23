2aca4024cb2185f50402f9d17af1df54c386f775
John Paul Taguinod <johnpaultaguinod30@gmail.com>
2025-07-23 13:30:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -396,7 +396,42 @@ if (isset($_POST['delete_avatar'])) {
         position: relative;
         left: -20px;
     }
-        
+    .bi-gear{
+        display: flex;
+        justify-content: left;
+        width:  16px;
+        height: 16px;
+        float: left;
+        position: relative;
+        left: -20px;
+        }
+    .bi-box-arrow-in-left{
+        display: flex;
+        justify-content: left;
+        width:  16px;
+        height: 16px;
+        float: left;
+        position: relative;
+        left: -20px;
+    }
+    .bi-cart4{
+        display: flex;
+        justify-content: left;
+        width:  16px;
+        height: 16px;
+        float: left;
+        position: relative;
+        left: -20px;
+    }
+    .bi-check2-circle{
+        display: flex;
+        justify-content: left;
+        width:  16px;
+        height: 16px;
+        float: left;
+        position: relative;
+        left: -20px;
+    }
     </style>
 </head>
 <body>
@@ -431,11 +466,29 @@ if (isset($_POST['delete_avatar'])) {
                 </form>
             </div>
             <ul class="menu">
-            <li>Updates</li>
-                <li>View Orders</li>
+            <li>Updates
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi-check2-circle" viewBox="0 0 16 16">
+            <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+            <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+            </svg>
+            </li>
+                <li>View Orders
+                <svg xmlns="http://www.w3.org/2000/svg"fill="currentColor" class="bi-cart4" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+                </svg>
+                </li>
+                
                     <li><a href="Account_setting.php" style="color: white; text-decoration: none;">Account Setting
-                        <li><a href="dash.php" style="color: white; text-decoration: none;">Logout</a></li>
-</ul>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-gear" viewBox="0 0 16 16">
+                <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
+                <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z"/>
+                </svg></li>
+                        <li><a href="dash.php" style="color: white; text-decoration: none;">Logout</a>
+                        <svg xmlns="http://www.w3.org/2000/svg"fill="currentColor" class="bi-box-arrow-in-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0z"/>
+                        <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                        </svg></li>
+                        </ul>
 
         </aside>
 
